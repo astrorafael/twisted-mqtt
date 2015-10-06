97437fd3b523b2e5650ceb1a951553ce6742f9db
Rafael Gonzalez <astrorafael@yahoo.es>
2015-10-06 17:53:23 +0200

    adding conversion from markdown to reStructuredText in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,34 @@
 from setuptools import setup, Extension, find_packages
 import versioneer
 
+long_description = ''
+ 
+try:
+    import subprocess
+    import pandoc
+ 
+    process = subprocess.Popen(
+        ['which pandoc'],
+        shell=True,
+        stdout=subprocess.PIPE,
+        universal_newlines=True
+    )
+ 
+    pandoc_path = process.communicate()[0]
+    pandoc_path = pandoc_path.strip('\n')
+ 
+    pandoc.core.PANDOC_PATH = pandoc_path
+ 
+    doc = pandoc.Document()
+    doc.markdown = open('README.md').read()
+ 
+    long_description = doc.rst
+ 
+except:
+    pass
+   
+
+
 classifiers = [
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
@@ -19,7 +47,7 @@ setup(name             = 'twisted-mqtt',
       author           = 'Rafael Gonzalez',
       author_email     = 'astrorafael@yahoo.es',
       description      = 'MQTT client protocol package for Twisted',
-      long_description = open('README').read(),
+      long_description = long_description,
       license          = 'MIT',
       keywords         = 'Python Twisted',
       url              = 'http://github.com/astrorafael/twisted-mqtt/',
