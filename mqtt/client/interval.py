687c8e3a7a50557c1aafd754f0b9fd85c00da33c
Rafael Gonzalez <astrorafael@yahoo.es>
2016-07-12 22:13:47 +0200

    added jitter to Intervals

--- mqtt/client/interval.py
+++ mqtt/client/interval.py
@@ -1,3 +1,5 @@
+import random
+
 class Interval(object):
     '''
     This class build automatically incrementing interval objects, 
@@ -13,14 +15,15 @@ class Interval(object):
     @cvar maxDelay: maximun interval value produced, in seconds.
     @cvar factor:   multiplier for the next interval.
     '''
-  
-    # Class attributes
-    initial  = 2    # seconds
-    maxDelay = 1024 # seconds
+    initial  = 2
     factor   = 2
-
-    def __init__(self):
+    maxDelay = 1024
+    
+    def __init__(self, initial=2, maxDelay=1024, factor=2):
         '''Initialize interval object'''
+        self.initial  = initial
+        self.factor   = factor
+        self.maxDelay = max(initial, maxDelay)
         self._value   = self.initial
 
 
@@ -28,4 +31,4 @@ class Interval(object):
         '''Call the interval with an id and produce a new value for that id'''
         self._value *= self.factor
         self._value = min(self._value, self.maxDelay)
-        return self._value
+        return self._value + random.random()
